e212122ec00996571f15d6b0cca52b8bc7b7d6ea
dailybetter <a40293865@gmail.com>
2023-01-23 17:49:25 +0900

    Feat: calendar addEvent 작업중

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,47 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
+import interactionPlugin from '@fullcalendar/interaction';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+
+// import CalendarSidebar from './CalendarSidebar';
 const Calendar = () => {
+  const handleDateSelect = (e) => {
+    console.log(e.view.calendar);
+    let title = prompt('Please enter a new title for your event');
+    let calendarApi = e.view.calendar;
+    calendarApi.unselect();
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: e.startStr,
+        end: e.endStr,
+        allDay: e.allDay,
+      });
+    }
+  };
+
+  const handleEvents = (e) => {
+    e.setState({
+      currentEvents: e,
+    });
+  };
+
   return (
     <>
+      {/* <CalendarSidebar /> */}
       <FullCalendar
-        defaultView='dayGridMonth'
-        plugins={[dayGridPlugin]}
-        events={[{ title: 'event 1', date: '2023-01-13' }]}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView='dayGridMonth'
+        editable={true}
+        selectable={true}
+        initialEvents={INITIAL_EVENTS}
+        eventsSet={(e) => {
+          handleEvents(e);
+        }}
+        dateClick={(e) => {
+          handleDateSelect(e);
+        }}
       />
     </>
   );
