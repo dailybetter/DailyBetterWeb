2934dee85a5f31f241c74091fe560c769f65567f
dailybetter <a40293865@gmail.com>
2023-01-25 00:07:34 +0900

    Feat:delete기능 구현 완료

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,30 +1,51 @@
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { createEventId } from './event-utils';
+// import { createEventId } from './event-utils';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { v4 as uuid } from 'uuid';
 // import CalendarSidebar from './CalendarSidebar';
 const Calendar = () => {
+  let toggle = true;
   const [events, setEvents] = useState([]);
   useEffect(() => {
     axios.get('http://localhost:3003/calendarEvents/').then((res) => {
-      setEvents(res);
+      setEvents(res.data);
     });
-  }, []);
+    console.log('랜더링');
+  }, [toggle]);
   const handleDateSelect = (e) => {
-    console.log(e.view.calendar);
+    console.log(e.view.calendar.addEvent);
     let title = prompt('Please enter a new title for your event');
     let calendarApi = e.view.calendar;
     calendarApi.unselect();
     if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: e.startStr,
-        end: e.endStr,
+      axios.post('http://localhost:3003/calendarEvents', {
         allDay: e.allDay,
+        end: e.endStr,
+        id: uuid(),
+        start: e.startStr,
+        title,
       });
+      calendarApi.addEvent({
+        allDay: e.allDay,
+        end: e.endStr,
+        id: uuid(),
+        start: e.startStr,
+        title,
+      });
+      toggle = !toggle;
+    }
+  };
+  const handleEventClick = (e) => {
+    console.log(e);
+    const eventTitle = e.el.fcSeg.eventRange.def.title;
+    const ID = e.el.fcSeg.eventRange.def.publicId;
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm(`${eventTitle}를 삭제하시겠습니까?`)) {
+      e.event.remove();
+      axios.delete(`http://localhost:3003/calendarEvents/${ID}`);
     }
   };
 
@@ -34,17 +55,17 @@ const Calendar = () => {
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView='dayGridMonth'
-        editable={true}
-        selectable={true}
-        initialEvents={events}
+        editable
+        selectable
+        // initialEvents={events}
         headerToolbar={{
           start: 'today prev next',
           end: 'dayGridMonth dayGridWeek dayGridDay',
         }}
         events={events}
-        // dateClick={(e) => {
-        //   handleDateSelect(e);
-        // }}
+        eventClick={(e) => {
+          handleEventClick(e);
+        }}
         select={(e) => {
           handleDateSelect(e);
         }}
